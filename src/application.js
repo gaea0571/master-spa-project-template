d0e73b965ff279615d70ca3af9214a21de5dfd66
yusc <yusc@gaea.cn>
2022-11-14 10:43:57 +0800

    temp:临时节点标记 星期一 上午 10:43:57 十一月 14日 2022 修改了 example/master-spa-project/src/application.js

--- src/application.js
+++ src/application.js
@@ -1,23 +1,35 @@
-import React from "react";
 import { Space } from "antd";
 import { NavLink } from "react-router-dom";
+import React, { useState, useCallback } from "react";
+
 
 import RegistryApplication from "@/elements/RegistryApplication";
 
 export function RootComponent() {
+
+  const [router_config, change_router] = useState([]);
+
+  const handleChangeRouter = useCallback((current_router_config) => {
+    change_router([...router_config, ...current_router_config]);
+  }, [router_config]);
+
   return (
     <div style={{ width: "100%", height: "100%" }}>
       <Space>
         <NavLink to="/">主页</NavLink>
         <NavLink to="slave">前往slave子应用</NavLink>
       </Space>
+      <div>
+        <div>装载的子应用路由</div>
+        <pre>{JSON.stringify(router_config, null, " ")}</pre>
+      </div>
       {Object.keys(process.env.slave_application_list).map((namespace) => {
         return (
           <RegistryApplication
             key={namespace}
             namespace={namespace}
             resource={process.env.slave_application_list[namespace]}
-            onRouterConfig={(router_config) => console.log(router_config)}
+            onRouterConfig={handleChangeRouter}
           />
         )
       })}
