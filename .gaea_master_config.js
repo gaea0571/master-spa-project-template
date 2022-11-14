2130e0ce93c232d79008bd4d6069fc07f46ed8d0
yusc <yusc@gaea.cn>
2022-11-14 09:47:10 +0800

    temp:临时节点标记 星期一 上午 9:47:09 十一月 14日 2022 修改了 example/master-spa-project/.gaea_master_config.js 修改了 packages/slave-spa/src/configs/webpack.basic.config.js 修改了 packages/slave-spa/src/configs/webpack.build.config.js 修改了 packages/slave-spa/src/configs/webpack.dev.config.js 修改了 packages/slave-spa/src/scripts/start_dev_server.js

--- .gaea_master_config.js
+++ .gaea_master_config.js
@@ -7,13 +7,13 @@ module.exports = {
   bundle_analyzer: false,
   slave_application_list: {
     "development": {
-      "slave": "http://localhost:18600/manifest.json"
+      "slave": "http://localhost:18600/v1/manifest.json"
     },
     "test": {
-      "slave": "http://localhost:18600/manifest.json"
+      "slave": "http://localhost:18600/v1/manifest.json"
     },
     "production": {
-      "slave": "http://localhost:18600/manifest.json"
+      "slave": "http://localhost:18600/v1/manifest.json"
     }
   }[process.env.NODE_ENV]
 };
